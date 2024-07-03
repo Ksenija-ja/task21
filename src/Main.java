faec9318b0fe89e3b5ce5737637e5b81e9472cfa
Ksenija <omega-2077@list.ru>
2024-07-03 19:07:20 +0300

    change task

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 15660;
+        int mile = 20;
+
+
+        System.out.println("итоговый бонус:" + (amount / mile));
+
+
+    }
+}
